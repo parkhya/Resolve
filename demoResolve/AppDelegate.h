7d5a0e0db3739623c9fcba1c341902025950a1a8
Swapnil Parkhya <sparkhya@gmail.com>
2015-01-02 09:53:38 +0530

    Initial Commit

---
+++ demoResolve/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoResolve
+//
+//  Created by Abc on 02/01/15.
+//  Copyright (c) 2015 Parkhya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
